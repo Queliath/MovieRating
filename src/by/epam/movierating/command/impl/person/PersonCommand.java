160b149d48e37ca2ed6e144fce6a8ccbfaac5a46
Vladislav <kostevichvladislav@gmail.com>
2016-07-25 19:53:07 +0300

    Added localization to person page and a part of person command

---
+++ src/by/epam/movierating/command/impl/person/PersonCommand.java
@@ -0,0 +1,78 @@
+package by.epam.movierating.command.impl.person;
+
+import by.epam.movierating.command.Command;
+import by.epam.movierating.command.util.LanguageUtil;
+import by.epam.movierating.command.util.QueryUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Владислав on 25.07.2016.
+ */
+public class PersonCommand implements Command {
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String idStr = request.getParameter("id");
+        int id = (idStr == null) ? -1 : Integer.parseInt(idStr);
+
+        if(id == -1){
+            response.sendRedirect("/Controller?command=welcome");
+            return;
+        }
+
+        QueryUtil.saveCurrentQueryToSession(request);
+        String languageId = LanguageUtil.getLanguageId(request);
+        setLocaleAttributes(request, languageId);
+    }
+
+    private void setLocaleAttributes(HttpServletRequest request, String languageId){
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("locale", new Locale(languageId));
+
+        request.setAttribute("siteName", resourceBundle.getString("locale.siteName"));
+        request.setAttribute("mainPageName", resourceBundle.getString("locale.mainPageName"));
+        request.setAttribute("catalogPageName", resourceBundle.getString("locale.catalogPageName"));
+        request.setAttribute("registrationPageName", resourceBundle.getString("locale.registrationPageName"));
+        request.setAttribute("loginPageName", resourceBundle.getString("locale.loginPageName"));
+        request.setAttribute("profilePageName", resourceBundle.getString("locale.profilePageName"));
+        request.setAttribute("logoutName", resourceBundle.getString("locale.logoutName"));
+        request.setAttribute("personsPageName", resourceBundle.getString("locale.personsPageName"));
+        request.setAttribute("usersPageName", resourceBundle.getString("locale.usersPageName"));
+        request.setAttribute("localeOther", resourceBundle.getString("locale.other"));
+        request.setAttribute("genresPageName", resourceBundle.getString("locale.genresPageName"));
+        request.setAttribute("countriesPageName", resourceBundle.getString("locale.countriesPageName"));
+        request.setAttribute("localeMoviesTotal", resourceBundle.getString("locale.moviesTotal"));
+        request.setAttribute("localeDateOfBirth", resourceBundle.getString("locale.dateOfBirth"));
+        request.setAttribute("localePlaceOfBirth", resourceBundle.getString("locale.placeOfBirth"));
+        request.setAttribute("localeCountry", resourceBundle.getString("locale.country"));
+        request.setAttribute("localeGenre", resourceBundle.getString("locale.genre"));
+        request.setAttribute("localeYear", resourceBundle.getString("locale.year"));
+        request.setAttribute("localeBudget", resourceBundle.getString("locale.budget"));
+        request.setAttribute("localePremiere", resourceBundle.getString("locale.premiere"));
+        request.setAttribute("localeLasting", resourceBundle.getString("locale.lasting"));
+        request.setAttribute("localeMinute", resourceBundle.getString("locale.minute"));
+        request.setAttribute("localeRating", resourceBundle.getString("locale.rating"));
+        request.setAttribute("localeServiceError", resourceBundle.getString("locale.serviceError"));
+        request.setAttribute("localeNoPerson", resourceBundle.getString("locale.noPerson"));
+        request.setAttribute("localeAsActor", resourceBundle.getString("locale.asActor"));
+        request.setAttribute("localeAsDirector", resourceBundle.getString("locale.asDirector"));
+        request.setAttribute("localeAsProducer", resourceBundle.getString("locale.asProducer"));
+        request.setAttribute("localeAsWriter", resourceBundle.getString("locale.asWriter"));
+        request.setAttribute("localeAsPainter", resourceBundle.getString("locale.asPainter"));
+        request.setAttribute("localeAsOperator", resourceBundle.getString("locale.asOperator"));
+        request.setAttribute("localeAsEditor", resourceBundle.getString("locale.asEditor"));
+        request.setAttribute("localeAsComposer", resourceBundle.getString("locale.asComposer"));
+        request.setAttribute("localeEdit", resourceBundle.getString("locale.edit"));
+        request.setAttribute("localeDelete", resourceBundle.getString("locale.delete"));
+        request.setAttribute("localeAddMovie", resourceBundle.getString("locale.addMovie"));
+        request.setAttribute("localeDeleteTitle", resourceBundle.getString("locale.deleteTitle"));
+        request.setAttribute("localeDeleteBody", resourceBundle.getString("locale.deleteBody"));
+        request.setAttribute("localeCancel", resourceBundle.getString("locale.cancel"));
+
+        request.setAttribute("selectedLanguage", languageId);
+    }
+}
