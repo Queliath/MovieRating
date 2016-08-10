a3d4720af9db5584229fe35b865f9531b6db4e6a
Vladislav <kostevichvladislav@gmail.com>
2016-08-10 22:20:43 +0300

    Made relation service

--- src/by/epam/movierating/service/impl/RelationServiceImpl.java
+++ src/by/epam/movierating/service/impl/RelationServiceImpl.java
@@ -1,5 +1,10 @@
 package by.epam.movierating.service.impl;
 
+import by.epam.movierating.dao.exception.DAOException;
+import by.epam.movierating.dao.factory.DAOFactory;
+import by.epam.movierating.dao.interfaces.MovieCountryDAO;
+import by.epam.movierating.dao.interfaces.MovieGenreDAO;
+import by.epam.movierating.dao.interfaces.MoviePersonRelationDAO;
 import by.epam.movierating.service.exception.ServiceException;
 import by.epam.movierating.service.interfaces.RelationService;
 
@@ -9,16 +14,34 @@ import by.epam.movierating.service.interfaces.RelationService;
 public class RelationServiceImpl implements RelationService {
     @Override
     public void addPersonToMovie(int movieId, int personId, int relationType) throws ServiceException {
-
+        try {
+            DAOFactory daoFactory = DAOFactory.getInstance();
+            MoviePersonRelationDAO moviePersonRelationDAO = daoFactory.getMoviePersonRelationDAO();
+            moviePersonRelationDAO.addMovieToPersonWithRelation(movieId, personId, relationType);
+        } catch (DAOException e) {
+            throw new ServiceException("Service layer: cannot add person to movie", e);
+        }
     }
 
     @Override
     public void addCountryToMovie(int movieId, int countryId) throws ServiceException {
-
+        try {
+            DAOFactory daoFactory = DAOFactory.getInstance();
+            MovieCountryDAO movieCountryDAO = daoFactory.getMovieCountryDAO();
+            movieCountryDAO.addMovieToCountry(movieId, countryId);
+        } catch (DAOException e) {
+            throw new ServiceException("Service layer: cannot add country to movie", e);
+        }
     }
 
     @Override
     public void addGenreToMovie(int movieId, int genreId) throws ServiceException {
-
+        try {
+            DAOFactory daoFactory = DAOFactory.getInstance();
+            MovieGenreDAO movieGenreDAO = daoFactory.getMovieGenreDAO();
+            movieGenreDAO.addMovieToGenre(movieId, genreId);
+        } catch (DAOException e) {
+            throw new ServiceException("Service layer: cannot add genre to movie", e);
+        }
     }
 }
