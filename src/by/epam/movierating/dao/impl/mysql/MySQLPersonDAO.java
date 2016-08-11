3b321637ae7cba4b67ae72e8c1756da7b574582b
Vladislav <kostevichvladislav@gmail.com>
2016-08-11 18:06:22 +0300

    Made a part of refactoring in Person DAO

--- src/by/epam/movierating/dao/impl/mysql/MySQLPersonDAO.java
+++ src/by/epam/movierating/dao/impl/mysql/MySQLPersonDAO.java
@@ -14,6 +14,36 @@ import java.util.List;
  * Created by Владислав on 11.06.2016.
  */
 public class MySQLPersonDAO implements PersonDAO {
+    private static final String ADD_PERSON_QUERY = "INSERT INTO person " +
+            "(name, date_of_birth, place_of_birth, photo) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_PERSON_QUERY = "UPDATE person " +
+            "SET name = ?, date_of_birth = ?, place_of_birth = ?, photo = ? WHERE id = ?";
+    private static final String TPERSON_CHECK_QUERY = "SELECT id FROM tperson " +
+            "WHERE language_id = ? AND id = ?";
+    private static final String ADD_TPERSON_QUERY = "INSERT INTO tperson " +
+            "(language_id, id, name, place_of_birth) VALUES (?, ?, ?, ?)";
+    private static final String UPDATE_TPERSON_QUERY = "UPDATE tperson " +
+            "SET name = ?, place_of_birth = ? WHERE language_id = ? AND id = ?";
+    private static final String DELETE_PERSON_QUERY = "DELETE FROM person WHERE id = ?";
+    private static final String GET_ALL_PERSONS_QUERY = "SELECT * FROM person";
+    private static final String GET_ALL_PERSONS_NOT_DEFAULT_LANG_QUERY = "SELECT p.id, " +
+            "coalesce(t.name, p.name), p.date_of_birth, coalesce(t.place_of_birth, p.place_of_birth), " +
+            "p.photo FROM person AS p LEFT JOIN (SELECT * FROM tperson WHERE language_id = ?) AS t " +
+            "USING(id) WHERE p.id = ?";
+    private static final String GET_PERSON_BY_ID_QUERY = "SELECT * FROM person WHERE id = ?";
+    private static final String GET_PERSON_BY_ID_NOT_DEFAULT_LANG_QUERY = "SELECT p.id, " +
+            "coalesce(t.name, p.name), p.date_of_birth, coalesce(t.place_of_birth, p.place_of_birth), " +
+            "p.photo FROM person AS p LEFT JOIN (SELECT * FROM tperson WHERE language_id = ?) AS t " +
+            "USING(id) WHERE p.id = ?";
+    private static final String GET_PERSONS_BY_MOVIE_QUERY = "SELECT person.* FROM person " +
+            "INNER JOIN movie_person_relation ON person.id = movie_person_relation.person_id WHERE " +
+            "movie_person_relation.movie_id = ? AND movie_person_relation.relation_type = ?";
+    private static final String GET_PERSONS_BY_MOVIE_NOT_DEFAULT_LANG_QUERY = "SELECT p.id, coalesce(t.name, p.name), p.date_of_birth, " +
+            "coalesce(t.place_of_birth, p.place_of_birth), p.photo FROM person AS p INNER JOIN " +
+            "movie_person_relation AS mpr ON p.id = mpr.person_id LEFT JOIN " +
+            "(SELECT * FROM tperson WHERE language_id = ?) AS t USING(id) " +
+            "WHERE mpr.movie_id = ? AND mpr.relation_type = ?";
+
     private static final String DEFAULT_LANGUAGE_ID = "EN";
 
     @Override
@@ -27,8 +57,7 @@ public class MySQLPersonDAO implements PersonDAO {
         }
 
         try{
-            PreparedStatement statement = connection.prepareStatement("INSERT INTO person " +
-                    "(name, date_of_birth, place_of_birth, photo) VALUES (?, ?, ?, ?)");
+            PreparedStatement statement = connection.prepareStatement(ADD_PERSON_QUERY);
             statement.setString(1, person.getName());
             statement.setDate(2, new Date(person.getDateOfBirth().getTime()));
             statement.setString(3, person.getPlaceOfBirth());
@@ -59,8 +88,7 @@ public class MySQLPersonDAO implements PersonDAO {
         try{
             PreparedStatement statement = null;
             if(languageId.equals(DEFAULT_LANGUAGE_ID)){
-                statement = connection.prepareStatement("UPDATE person " +
-                        "SET name = ?, date_of_birth = ?, place_of_birth = ?, photo = ? WHERE id = ?");
+                statement = connection.prepareStatement(UPDATE_PERSON_QUERY);
                 statement.setString(1, person.getName());
                 statement.setDate(2, new Date(person.getDateOfBirth().getTime()));
                 statement.setString(3, person.getPlaceOfBirth());
@@ -68,22 +96,19 @@ public class MySQLPersonDAO implements PersonDAO {
                 statement.setInt(5, person.getId());
             }
             else {
-                PreparedStatement checkStatement = connection.prepareStatement("SELECT id FROM tperson " +
-                        "WHERE language_id = ? AND id = ?");
+                PreparedStatement checkStatement = connection.prepareStatement(TPERSON_CHECK_QUERY);
                 checkStatement.setString(1, languageId);
                 checkStatement.setInt(2, person.getId());
                 ResultSet resultSet = checkStatement.executeQuery();
                 if(resultSet.next()){
-                    statement = connection.prepareStatement("UPDATE tperson " +
-                            "SET name = ?, place_of_birth = ? WHERE language_id = ? AND id = ?");
+                    statement = connection.prepareStatement(UPDATE_TPERSON_QUERY);
                     statement.setString(1, person.getName());
                     statement.setString(2, person.getPlaceOfBirth());
                     statement.setString(3, languageId);
                     statement.setInt(4, person.getId());
                 }
                 else {
-                    statement = connection.prepareStatement("INSERT INTO tperson " +
-                            "(language_id, id, name, place_of_birth) VALUES (?, ?, ?, ?)");
+                    statement = connection.prepareStatement(ADD_TPERSON_QUERY);
                     statement.setString(1, languageId);
                     statement.setInt(2, person.getId());
                     statement.setString(3, person.getName());
@@ -114,7 +139,7 @@ public class MySQLPersonDAO implements PersonDAO {
         }
 
         try{
-            PreparedStatement statement = connection.prepareStatement("DELETE FROM person WHERE id = ?");
+            PreparedStatement statement = connection.prepareStatement(DELETE_PERSON_QUERY);
             statement.setInt(1, id);
 
             statement.executeUpdate();
@@ -143,13 +168,10 @@ public class MySQLPersonDAO implements PersonDAO {
             ResultSet resultSet = null;
             if(languageId.equals(DEFAULT_LANGUAGE_ID)){
                 Statement statement = connection.createStatement();
-                resultSet = statement.executeQuery("SELECT * FROM person");
+                resultSet = statement.executeQuery(GET_ALL_PERSONS_QUERY);
             }
             else {
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT p.id, " +
-                        "coalesce(t.name, p.name), p.date_of_birth, coalesce(t.place_of_birth, p.place_of_birth), " +
-                        "p.photo FROM person AS p LEFT JOIN (SELECT * FROM tperson WHERE language_id = ?) AS t " +
-                        "USING(id)");
+                PreparedStatement preparedStatement = connection.prepareStatement(GET_ALL_PERSONS_NOT_DEFAULT_LANG_QUERY);
                 preparedStatement.setString(1, languageId);
                 resultSet = preparedStatement.executeQuery();
             }
@@ -191,14 +213,11 @@ public class MySQLPersonDAO implements PersonDAO {
         try{
             PreparedStatement statement = null;
             if(languageId.equals(DEFAULT_LANGUAGE_ID)){
-                statement = connection.prepareStatement("SELECT * FROM person WHERE id = ?");
+                statement = connection.prepareStatement(GET_PERSON_BY_ID_QUERY);
                 statement.setInt(1, id);
             }
             else {
-                statement = connection.prepareStatement("SELECT p.id, " +
-                        "coalesce(t.name, p.name), p.date_of_birth, coalesce(t.place_of_birth, p.place_of_birth), " +
-                        "p.photo FROM person AS p LEFT JOIN (SELECT * FROM tperson WHERE language_id = ?) AS t " +
-                        "USING(id) WHERE p.id = ?");
+                statement = connection.prepareStatement(GET_PERSON_BY_ID_NOT_DEFAULT_LANG_QUERY);
                 statement.setString(1, languageId);
                 statement.setInt(2, id);
             }
@@ -238,18 +257,12 @@ public class MySQLPersonDAO implements PersonDAO {
         try{
             PreparedStatement statement = null;
             if(languageId.equals(DEFAULT_LANGUAGE_ID)){
-                statement = connection.prepareStatement("SELECT person.* FROM person " +
-                        "INNER JOIN movie_person_relation ON person.id = movie_person_relation.person_id WHERE " +
-                        "movie_person_relation.movie_id = ? AND movie_person_relation.relation_type = ?");
+                statement = connection.prepareStatement(GET_PERSONS_BY_MOVIE_QUERY);
                 statement.setInt(1, movieId);
                 statement.setInt(2, relationType);
             }
             else {
-                statement = connection.prepareStatement("SELECT p.id, coalesce(t.name, p.name), p.date_of_birth, " +
-                        "coalesce(t.place_of_birth, p.place_of_birth), p.photo FROM person AS p INNER JOIN " +
-                        "movie_person_relation AS mpr ON p.id = mpr.person_id LEFT JOIN " +
-                        "(SELECT * FROM tperson WHERE language_id = ?) AS t USING(id) " +
-                        "WHERE mpr.movie_id = ? AND mpr.relation_type = ?");
+                statement = connection.prepareStatement(GET_PERSONS_BY_MOVIE_NOT_DEFAULT_LANG_QUERY);
                 statement.setString(1, languageId);
                 statement.setInt(2, movieId);
                 statement.setInt(3, relationType);
