32c130cba6642507d9c6012df4378c5eb7cf032a
Vladislav <kostevichvladislav@gmail.com>
2016-09-17 16:23:04 +0300

    Added a general JUnit tests for the DAO layer

--- test/by/epam/movierating/dao/pool/mysql/MySQLConnectionPoolTest.java
+++ test/by/epam/movierating/dao/pool/mysql/MySQLConnectionPoolTest.java
@@ -1,7 +1,26 @@
 package by.epam.movierating.dao.pool.mysql;
 
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Created by Владислав on 15.09.2016.
  */
 public class MySQLConnectionPoolTest {
+    @Test(expected = MySQLConnectionPoolException.class)
+    public void freeOutsideConnectionTest() throws MySQLConnectionPoolException, InterruptedException, ClassNotFoundException, SQLException {
+        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
+        Connection poolConnection = mySQLConnectionPool.getConnection();
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection outsideConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/epam_movie_rating",
+                    "root", "root");
+            mySQLConnectionPool.freeConnection(outsideConnection);
+        } finally {
+            mySQLConnectionPool.freeConnection(poolConnection);
+        }
+    }
 }
