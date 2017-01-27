27083c29d8bcae9b889995e7dcded15e5a6ecfad
Uladzislau Kastsevich <uladzislau_kastsevich@epam.com>
2017-01-27 18:28:13 +0300

    updated rating dao

--- src/main/java/by/bsuir/movierating/dao/impl/MySQLRatingDAO.java
+++ src/main/java/by/bsuir/movierating/dao/impl/MySQLRatingDAO.java
@@ -1,16 +1,16 @@
 package by.bsuir.movierating.dao.impl;
 
 import by.bsuir.movierating.dao.RatingDAO;
-import by.bsuir.movierating.dao.exception.DAOException;
-import by.bsuir.movierating.dao.pool.mysql.MySQLConnectionPool;
-import by.bsuir.movierating.dao.pool.mysql.MySQLConnectionPoolException;
 import by.bsuir.movierating.domain.Rating;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,101 +19,57 @@ import java.util.List;
  * @author Kostevich Vladislav
  * @version 1.0
  */
+@Repository("ratingDao")
 public class MySQLRatingDAO implements RatingDAO {
     private static final String ADD_RATING_QUERY = "INSERT INTO rating " +
-            "(movie_id, user_id, value) VALUES (?, ?, ?)";
+            "(movie_id, user_id, value) VALUES (:movie_id, :user_id, :value)";
     private static final String UPDATE_RATING_QUERY = "UPDATE rating " +
-            "SET value = ? WHERE movie_id = ? AND user_id = ?";
-    private static final String DELETE_RATING_QUERY = "DELETE FROM rating WHERE movie_id = ? AND user_id = ?";
-    private static final String GET_RATING_BY_MOVIE_AND_USER_QUERY = "SELECT * FROM rating WHERE movie_id = ? AND user_id = ?";
+            "SET value = :value WHERE movie_id = :movie_id AND user_id = :movie_id";
+    private static final String DELETE_RATING_QUERY = "DELETE FROM rating WHERE movie_id = :movie_id AND user_id = :user_id";
+    private static final String GET_RATING_BY_MOVIE_AND_USER_QUERY = "SELECT * FROM rating WHERE movie_id = :movie_id AND user_id = :user_id";
     private static final String GET_AVERAGE_RATING_BY_MOVIE_QUERY = "SELECT AVG(value) " +
-            "FROM rating WHERE movie_id = ? GROUP BY movie_id";
-    private static final String GET_RATINGS_BY_USER_QUERY = "SELECT * FROM rating WHERE user_id = ?";
+            "FROM rating WHERE movie_id = :movie_id GROUP BY movie_id";
+    private static final String GET_RATINGS_BY_USER_QUERY = "SELECT * FROM rating WHERE user_id = :user_id";
+
+    private static final String MOVIE_ID_COLUMN_NAME = "movie_id";
+    private static final String USER_ID_COLUMN_NAME = "user_id";
+    private static final String VALUE_ID_COLUMN_NAME = "value";
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private RatingMapper ratingMapper = new RatingMapper();
+
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
 
     /**
      * Adds a rating to the data storage.
      *
      * @param rating a rating object
-     * @throws DAOException
      */
     @Override
-    public void addRating(Rating rating) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public void addRating(Rating rating) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue(MOVIE_ID_COLUMN_NAME, rating.getMovieId())
+                .addValue(USER_ID_COLUMN_NAME, rating.getUserId())
+                .addValue(VALUE_ID_COLUMN_NAME, rating.getValue());
 
-            statement = connection.prepareStatement(ADD_RATING_QUERY);
-            statement.setInt(1, rating.getMovieId());
-            statement.setInt(2, rating.getUserId());
-            statement.setInt(3, rating.getValue());
-
-            statement.executeUpdate();
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when adding rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
-        }
+        jdbcTemplate.update(ADD_RATING_QUERY, sqlParameterSource);
     }
 
     /**
      * Updates a rating in the data storage.
      *
      * @param rating a rating object
-     * @throws DAOException
      */
     @Override
-    public void updateRating(Rating rating) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public void updateRating(Rating rating) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue(MOVIE_ID_COLUMN_NAME, rating.getMovieId())
+                .addValue(USER_ID_COLUMN_NAME, rating.getUserId())
+                .addValue(VALUE_ID_COLUMN_NAME, rating.getValue());
 
-            statement = connection.prepareStatement(UPDATE_RATING_QUERY);
-            statement.setInt(1, rating.getValue());
-            statement.setInt(2, rating.getMovieId());
-            statement.setInt(3, rating.getUserId());
-
-            statement.executeUpdate();
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when updating rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
-        }
+        jdbcTemplate.update(UPDATE_RATING_QUERY, sqlParameterSource);
     }
 
     /**
@@ -121,42 +77,14 @@ public class MySQLRatingDAO implements RatingDAO {
      *
      * @param movieId an id of the movie to which the rating belongs
      * @param userId an id of the user to which the rating belongs
-     * @throws DAOException
      */
     @Override
-    public void deleteRating(int movieId, int userId) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public void deleteRating(int movieId, int userId) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue(MOVIE_ID_COLUMN_NAME, movieId)
+                .addValue(USER_ID_COLUMN_NAME, userId);
 
-            statement = connection.prepareStatement(DELETE_RATING_QUERY);
-            statement.setInt(1, movieId);
-            statement.setInt(2, userId);
-
-            statement.executeUpdate();
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when deleting rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
-        }
+        jdbcTemplate.update(DELETE_RATING_QUERY, sqlParameterSource);
     }
 
     /**
@@ -165,50 +93,15 @@ public class MySQLRatingDAO implements RatingDAO {
      * @param movieId an id of the movie
      * @param userId an id of the user
      * @return a rating belonging to the movie and the user
-     * @throws DAOException
      */
     @Override
-    public Rating getRatingByMovieAndUser(int movieId, int userId) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public Rating getRatingByMovieAndUser(int movieId, int userId) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource()
+                .addValue(MOVIE_ID_COLUMN_NAME, movieId)
+                .addValue(USER_ID_COLUMN_NAME, userId);
 
-            statement = connection.prepareStatement(GET_RATING_BY_MOVIE_AND_USER_QUERY);
-            statement.setInt(1, movieId);
-            statement.setInt(2, userId);
-            ResultSet resultSet = statement.executeQuery();
-
-            Rating rating = null;
-            if(resultSet.next()){
-                rating = new Rating();
-                rating.setMovieId(resultSet.getInt(1));
-                rating.setUserId(resultSet.getInt(2));
-                rating.setValue(resultSet.getInt(3));
-            }
-            return rating;
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when getting rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
-        }
+        List<Rating> ratings = jdbcTemplate.query(GET_RATING_BY_MOVIE_AND_USER_QUERY, sqlParameterSource, ratingMapper);
+        return ratings.isEmpty() ? null : ratings.get(0);
     }
 
     /**
@@ -216,46 +109,12 @@ public class MySQLRatingDAO implements RatingDAO {
      *
      * @param movieId an id of the movie
      * @return an average rating of the movie
-     * @throws DAOException
      */
     @Override
-    public double getAverageRatingByMovie(int movieId) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public double getAverageRatingByMovie(int movieId) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource(MOVIE_ID_COLUMN_NAME, movieId);
 
-            statement = connection.prepareStatement(GET_AVERAGE_RATING_BY_MOVIE_QUERY);
-            statement.setInt(1, movieId);
-            ResultSet resultSet = statement.executeQuery();
-
-            double averageRating = 0.0;
-            if(resultSet.next()){
-                averageRating = resultSet.getDouble(1);
-            }
-            return averageRating;
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when getting rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
-        }
+        return jdbcTemplate.queryForObject(GET_AVERAGE_RATING_BY_MOVIE_QUERY, sqlParameterSource, Double.TYPE);
     }
 
     /**
@@ -263,50 +122,22 @@ public class MySQLRatingDAO implements RatingDAO {
      *
      * @param userId an id of the user
      * @return a ratings belonging to the user
-     * @throws DAOException
      */
     @Override
-    public List<Rating> getRatingsByUser(int userId) throws DAOException {
-        MySQLConnectionPool mySQLConnectionPool = MySQLConnectionPool.getInstance();
-        Connection connection = null;
-        PreparedStatement statement = null;
-        try {
-            connection = mySQLConnectionPool.getConnection();
+    public List<Rating> getRatingsByUser(int userId) {
+        SqlParameterSource sqlParameterSource = new MapSqlParameterSource(USER_ID_COLUMN_NAME, userId);
 
-            statement = connection.prepareStatement(GET_RATINGS_BY_USER_QUERY);
-            statement.setInt(1, userId);
-            ResultSet resultSet = statement.executeQuery();
+        return jdbcTemplate.query(GET_RATINGS_BY_USER_QUERY, sqlParameterSource, ratingMapper);
+    }
 
-            List<Rating> ratingsByUser = new ArrayList<>();
-            while(resultSet.next()){
-                Rating rating = new Rating();
-                rating.setMovieId(resultSet.getInt(1));
-                rating.setUserId(resultSet.getInt(2));
-                rating.setValue(resultSet.getInt(3));
-
-                ratingsByUser.add(rating);
-            }
-            return ratingsByUser;
-        } catch (InterruptedException | MySQLConnectionPoolException e) {
-            throw new DAOException("Cannot get a connection from Connection Pool", e);
-        } catch (SQLException e) {
-            throw new DAOException("Exception in DAO layer when getting rating", e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException e) {
-                throw new DAOException("Cannot free a connection from Connection Pool", e);
-            } finally {
-                if (connection != null){
-                    try {
-                        mySQLConnectionPool.freeConnection(connection);
-                    } catch (SQLException | MySQLConnectionPoolException e) {
-                        throw new DAOException("Cannot free a connection from Connection Pool", e);
-                    }
-                }
-            }
+    private class RatingMapper implements RowMapper<Rating> {
+        @Override
+        public Rating mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Rating rating = new Rating();
+            rating.setMovieId(rs.getInt(MOVIE_ID_COLUMN_NAME));
+            rating.setUserId(rs.getInt(USER_ID_COLUMN_NAME));
+            rating.setValue(rs.getInt(VALUE_ID_COLUMN_NAME));
+            return rating;
         }
     }
 }
